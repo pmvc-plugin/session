16f58cf4b5fcb1111223f935a5e6a66e4448ed8d
Hill <hill@kimo.com>
2015-09-05 16:23:00 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+[![Latest Stable Version](https://poser.pugx.org/pmvc-plugin/session/v/stable)](https://packagist.org/packages/pmvc-plugin/session) 
+[![Latest Unstable Version](https://poser.pugx.org/pmvc-plugin/session/v/unstable)](https://packagist.org/packages/pmvc-plugin/session) 
+[![Build Status](https://travis-ci.org/pmvc-plugin/session.svg?branch=master)](https://travis-ci.org/pmvc-plugin/session)
+[![License](https://poser.pugx.org/pmvc-plugin/session/license)](https://packagist.org/packages/pmvc-plugin/session)
+[![Total Downloads](https://poser.pugx.org/pmvc-plugin/session/downloads)](https://packagist.org/packages/pmvc-plugin/session) 
+
+session
+===============
+
+## Install with Composer
+### 1. Download composer
+   * mkdir test_folder
+   * curl -sS https://getcomposer.org/installer | php
+
+### 2. Install by composer.json or use command-line directly
+#### 2.1 Install by composer.json
+   * vim composer.json
+```
+{
+    "require": {
+        "pmvc-plugin/session": "dev-master"
+    }
+}
+```
+   * php composer.phar install
+
+#### 2.2 Or use composer command-line
+   * php composer.phar require pmvc-plugin/session
+
---
+++ session.php
@@ -0,0 +1,59 @@
+<?php
+namespace PMVC\PlugIn\session;
+
+// \PMVC\l(__DIR__.'/xxx.php');
+
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\session';
+
+class session extends \PMVC\PlugIn
+    implements \SessionHandlerInterface
+{
+    public function init()
+    {
+        session_set_save_handler($this, true);
+        session_start();
+    }
+
+    public function open( $save_path , $session_name )
+    {
+    }
+
+    public function close()
+    {
+    }
+
+    public function read( $session_id )
+    {
+        $curl = \PMVC\plug('curl');
+        $url = \PMVC\getOption('INTERNAL').'/session/'.$session_id;
+        $return = '';
+        $curl->get($url, function($serverRespond) use (&$return){
+            $arr = json_decode($serverRespond->body); 
+            $return = $arr->session;
+        });
+        $curl->run();
+        return $return;
+    }
+
+    public function write($session_id , $session_data )
+    {
+        $this[$session_id] = $session_data;
+        $curl = \PMVC\plug('curl');
+        $url = \PMVC\getOption('INTERNAL').'/session/'.$session_id;
+        $curl->post($url, null, array('data'=>$session_data));
+        $curl->run();
+    }
+
+    public function destroy( $session_id )
+    {
+        $curl = \PMVC\plug('curl');
+        $url = \PMVC\getOption('INTERNAL').'/session/'.$session_id;
+        $curl->delete($url);
+        $curl->run();
+    }
+
+    public function gc( $maxlifetime )
+    {
+        return true;
+    }
+}
