d77e173d09f59e98d956d6e1154329730c2be871
Hill <hill@kimo.com>
2017-03-18 18:41:11 +0000

    improve session cookie

--- session.php
+++ session.php
@@ -9,6 +9,9 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\session';
 
 class session extends PlugIn
 {
+    /**
+     * Session config http://php.net/manual/en/session.configuration.php
+     */
     public function init()
     {
         if ($this['saveHandler']) {
@@ -17,6 +20,10 @@ class session extends PlugIn
         if ($this['name']) {
             session_name($this['name']);
         }
+        $this['cookie'] = array_replace(
+            $this->defaultCookie(),
+            \PMVC\get($this, 'cookie', [])
+        );
         $cookie = \PMVC\get($_COOKIE,$this['name']);
         if (!empty($cookie)) {
             $this->start();
@@ -26,8 +33,33 @@ class session extends PlugIn
     public function start()
     {
         if (empty($this['disableStart'])) {
+            $cParams = $this['cookie'];
+            call_user_func_array(
+                'session_set_cookie_params',
+                $cParams 
+            );
             session_start();
+            setcookie(
+                session_name(),
+                session_id(),
+                time()+$cParams['lifetime'],
+                $cParams['path'],
+                $cParams['domain'],
+                $cParams['secure'],
+                $cParams['httponly']
+            );
             $this['disableStart'] = true;
         }
+    }
+
+    public function defaultCookie()
+    {
+        return [
+            'lifetime'=>86400*7,
+            'path'=>'/',
+            'domain'=>null,
+            'secure'=>false,
+            'httponly'=>true
+        ];
     }
 }
